521f975bd8fda6e0d5a0d4d55ff099f80b44cc66
Sayan <sayanmunshi36@gmail.com>
2022-02-28 22:34:28 +0530

    credits given

--- components/Footer.js
+++ components/Footer.js
@@ -13,6 +13,7 @@ const Footer = () => {
         Copyright &copy; {date.getFullYear()} Mundhra Garden, Madhyamgram, North
         24 Pgs, West Bengal - All Rights Reserved
       </p>
+      <p className={footerStyles.footer_lowest_txt}>Designed and Built by Sayan Munshi</p>
       <Link passHref={true} href="#header">
         <button className={footerStyles.upBtn}>&uarr;</button>
       </Link>
